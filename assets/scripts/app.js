7a91a98dafa17035e105ed5065f12bd2c64a062b
John Meguerian <johnmeguerian@gmail.com>
2019-02-26 15:53:04 -0500

    tweak metronom timing

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,13 +2,13 @@ console.log('👋 hello from the javascript file.')
 
 const easings = document.querySelectorAll('.easing')
 
-easings.forEach(elem => {
-  elem.addEventListener('mouseover', ({ target }) => {
-    target.classList.add('active')
-    // document.documentElement.classList.add('should-fade')
-  })
-  elem.addEventListener('mouseout', ({ target }) => {
-    target.classList.remove('active')
-    // document.documentElement.classList.remove('should-fade')
-  })
-})
+// easings.forEach(elem => {
+//   elem.addEventListener('mouseover', ({ target }) => {
+//     target.classList.toggle('active')
+//     // document.documentElement.classList.add('should-fade')
+//   })
+//   elem.addEventListener('mouseout', ({ target }) => {
+//     target.classList.remove('active')
+//     void target.offsetWidth
+//   })
+// })
