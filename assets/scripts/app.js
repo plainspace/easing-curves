15616c55e0443bd17589d9f34ecef3d34a72e303
John Meguerian <johnmeguerian@gmail.com>
2019-02-27 14:44:26 -0500

    add initial pass at copy to clipboard alert

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,20 @@
 console.log('👋 hello from the javascript file.')
+
+const easings = document.querySelectorAll('.easing')
+
+function handleClick(el) {
+  // show alert
+  el.classList.add('clicked')
+
+  // copy to clipboard
+
+  // remove alert
+  setTimeout(() => {
+    el.classList.remove('clicked')
+  }, 800)
+}
+
+// listen for clicks
+easings.forEach(el => {
+  el.addEventListener('click', evt => handleClick(evt.target))
+})
