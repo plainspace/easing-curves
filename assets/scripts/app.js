f962fa91ebd9c1a0a425402cf4c6ec7eb5f982a8
John Meguerian <johnmeguerian@gmail.com>
2019-02-26 19:44:35 -0500

    remove js content

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,1 @@
 console.log('👋 hello from the javascript file.')
-
-const easings = document.querySelectorAll('.easing')
-
-// easings.forEach(elem => {
-//   elem.addEventListener('mouseover', ({ target }) => {
-//     target.classList.toggle('active')
-//     // document.documentElement.classList.add('should-fade')
-//   })
-//   elem.addEventListener('mouseout', ({ target }) => {
-//     target.classList.remove('active')
-//     void target.offsetWidth
-//   })
-// })
